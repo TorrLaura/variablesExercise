c231b859ee847e01270a490021e5a2f2356f18df
“TorrLaura” <laplebe35@gmail.com>
2021-09-22 09:06:02 -0500

    Reviewed variablesExercise and completed bonus in class review.

--- script.js
+++ script.js
@@ -15,7 +15,15 @@ let undefinedBlank;
 console.log(undefinedBlank);
 undefinedBlank = 30;
 console.log(30);
+//Bonus
 
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+
+console.log(quarter, dime, nickel, penny);
+
+let letters = "qwertyuiop";
+
+console.log(letters[letters.length - 1]);
 
 
 
