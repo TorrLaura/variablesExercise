0e540d0e5e10bfa1ce512f94a18d0bd1476b922d
“TorrLaura” <laplebe35@gmail.com>
2021-09-21 19:56:02 -0500

    Completed Variables Exercise

---
+++ script.js
@@ -0,0 +1,27 @@
+//Const (Used for variables with a value that will not change)
+let num = 20;
+console.log(num);
+num = 15;
+console.log(num)
+num = num + 2;
+console.log(num);
+num = num + 5;
+console.log(num);
+num --;
+console.log(num);
+num = "This is not a number"
+console.log(num);
+let undefinedBlank;
+console.log(undefinedBlank);
+undefinedBlank = 30;
+console.log(30);
+
+
+
+
+
+
+
+
+
+
